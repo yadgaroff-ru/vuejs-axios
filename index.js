548b0696397df855ca4e3a57dbac25628a8d073f
Nikita Yadgaroff <web.real.sense@gmail.com>
2023-04-30 22:19:51 +0300

    plugin v.1

---
+++ index.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+
+const Vue2Axios = {}
+
+Vue2Axios.install = (Vue, options, promises) => {
+  const handlers = {
+    request: {
+      done: (request) => request,
+      fail: (err) => Promise.reject(err),
+    },
+    response: {
+      done: (response) => response,
+      fail: (err) => Promise.reject(err),
+    },
+  }
+
+  if (promises) {
+    if (promises.hasOwnProperty('request')) {
+      handlers.request = { ...handlers.request, ...promises.request }
+    }
+
+    if (promises.hasOwnProperty('response')) {
+      handlers.response = { ...handlers.response, ...promises.response }
+    }
+  }
+
+  // Создание инстанса
+  const service = axios.create(options)
+
+  // https://axios-http.com/ru/docs/interceptors
+  // Перехват запроса
+  service.interceptors.request.use(
+    (request) => handlers.request.done(request),
+    (error) => handlers.request.fail(error)
+  )
+
+  // Перехват ответа
+  service.interceptors.response.use(
+    (response) => handlers.response.done(response),
+    (error) => handlers.response.fail(error)
+  )
+
+  Vue.prototype.$axios = service
+}
+
+export default Vue2Axios
